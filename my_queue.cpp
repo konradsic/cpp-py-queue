34a9cb955841efee404d98cec053c839ee007e21
konradsic <102176820+konradsic@users.noreply.github.com>
2022-03-29 19:00:38 +0200

    Basic Queue Class

--- my_queue.cpp
+++ my_queue.cpp
@@ -1,16 +1,24 @@
 /*
-	Class implementing queue in C++
-	Same functionality as STL (Standart Template Liblary) class queue in C++
-	With usage example
-	Version: v1.0
-	Consists:
-		a) Program template
-	Author: Konrad S. (konradsic)
+Class implementing queue in C++
+Same functionality as STL (Standart Template Liblary) class queue in C++
+With usage example
+Version: v1.0
+Consists:
+	a) Program template
+Author: Konrad S. (konradsic)
+Project created on OKI 2022 (https://oki.org.pl)
 */
 
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class mQueue {
+	public:
+	private:
+	vector<int> queue;
+	int first,last;
+};
 
 int main() {
 	
