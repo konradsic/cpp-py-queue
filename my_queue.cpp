2b4a2fc4065b8430c7d48ceacec4b7e243a63b76
konradsic <102176820+konradsic@users.noreply.github.com>
2022-03-29 18:42:11 +0200

    Creates basic file

---
+++ my_queue.cpp
@@ -0,0 +1,18 @@
+/*
+	Class implementing queue in C++
+	Same functionality as STL (Standart Template Liblary) class queue in C++
+	With usage example
+	Version: v1.0
+	Consists:
+		a) Program template
+	Author: Konrad S. (konradsic)
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
