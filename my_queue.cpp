13d2935d7a84c0b26e0af94d1d36764a154b0c2e
konradsic <102176820+konradsic@users.noreply.github.com>
2022-03-29 20:19:41 +0200

    Final Version

--- my_queue.cpp
+++ my_queue.cpp
@@ -1,26 +1,76 @@
 /*
-Class implementing queue in C++
-Same functionality as STL (Standart Template Liblary) class queue in C++
-With usage example
-Version: v1.0
-Consists:
-	a) Program template
+//class implementing queue in C++
+//the same functionality as STL (Standard Template Library ) class queue in C++ 
+//with usage example
+//Version 1.5
+Consists
+a. Program template
+b. defintion of class mqueue 
+c. Constructor
+d. empty() method
+e. push() method
+f. dequeue() method
 Author: Konrad S. (konradsic)
-Project created on OKI 2022 (https://oki.org.pl)
-*/
-
+Made on OKI 2022 (https://oki.org.pl)
+ */
+ 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-class mQueue {
-	public:
-	private:
-	vector<int> queue;
-	int first,last;
-};
+class mqueue {
+public:
+ mqueue() { 
+    first = last = -1;
+ } 
+ bool empty() {
+    if (first == -1 ) 
+        return true; 
+    return false; 
+ }	
+ void push(int value) {
+    if (  empty() == true ) {
+       elements.push_back(value);
+	   first = last = 0;
+	   return;
+    }
+    elements.push_back(value);
+    ++last;
+ }	
+ int dequeue() {
+   int value;
+   if ( empty() ) {
+      return value;
+   }
+   if ( first == last) {
+      value = elements[first];
+      first = last = -1;
+	   elements.clear();
+	  return value;
+   }
+   value = elements[first];
+   ++first;
+   return value;
+ }
+private:
+ vector<int> elements;
+ int first, last;
+};	
 
 int main() {
-	
-	return 0;
+ mqueue	payments;
+ int value;
+ 
+ cout << "empty?: " << payments.empty() << "\n";
+ payments.push(5);
+ payments.push(-3);
+ payments.push(8);
+ cout << "empty?: " << payments.empty() << "\n";
+ cout << "serviced: " << payments.dequeue() << "\n";
+ cout << "serviced: " << payments.dequeue() << "\n";
+ payments.push(11);
+ cout << "serviced: " << payments.dequeue() << "\n";
+ cout << "serviced: " << payments.dequeue() << "\n";
+ cout << "empty?: " << payments.empty() << "\n";
+ return 0;
 }
